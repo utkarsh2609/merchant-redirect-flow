a48571d62f51d81befd03e9b68e7884789bb4322
utkarsh2609 <utkarsh.awasthi01@gmail.com>
2024-10-20 16:06:04 +0530

    react query initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { RouterProvider } from 'react-router-dom'
 import routes from './appRoutes.js';
 
-import './App.css'
+import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+// Create a client
+const queryClient = new QueryClient()
 
 function App() {
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={routes} />
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+    // <>
+    // </>
   )
 }
 
